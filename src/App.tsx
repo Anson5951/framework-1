5a98ef5b817a19f632319fea69269d2ba612da81
anson <message9698@gmail.com>
2025-12-07 22:25:27 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+	return (
+		<>
+			<Button onClick={() => console.log("Test")}>Test Button</Button>
+		</>
+	)
+}
+
+export default App
