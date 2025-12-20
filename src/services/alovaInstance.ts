4e3b734094101f462d7c90a04fa74404929e3630
Junyu <happystyle666@gmail.com>
2025-12-20 16:11:44 +0800

    uninstall alova package and related files

--- src/services/alovaInstance.ts
+++
@@ -1,8 +0,0 @@
-import { createAlova } from 'alova';
-import adapterFetch from 'alova/fetch';
-
-export const noCacheAlovaInstance = createAlova({
-	requestAdapter: adapterFetch(),
-	responded: response => response.json(),
-	cacheFor: null
-});
--- src/services/testService.ts
+++
@@ -1,8 +0,0 @@
-import type { Joke } from "@/types/Joke";
-import { noCacheAlovaInstance } from "./alovaInstance";
-
-
-export const testService = {
-	getJoke: () =>
-		noCacheAlovaInstance.Get<Joke>('https://v2.jokeapi.dev/joke/Programming')
-};
